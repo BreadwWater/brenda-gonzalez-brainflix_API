0c66d126108dcc823199ceea56fb1c4e0d02c0a1
BreadwWater <bren.gonzalez1926@gmail.com>
2023-04-23 15:07:13 -0400

    major| Near completion of backend API functionality; all done except upload new data

--- server.js
+++ server.js
@@ -1,26 +1,35 @@
-// initialize Express Server in project
-const express = require('express');
+// Importing required modules and packages
+const express = require(`express`);
 const app = express();
+const cors = require(`cors`);
+const path = require(`path`);
+const Port = 8080;
 
-// when the server receives a GET request to '/'
-app.get('/', (req, res) => {
-    // send some text back as a response
-    res.send('Express is running!');
+// Import the videos route handler
+const Routevideos = require(`./routes/Videos`);
+
+// Mount the videos route at /videos
+app.use(`/videos`, Routevideos);
+
+// Serve static files in the /public directory at /public
+app.use(`/public`, express.static(path.join(__dirname, `public`)));
+
+// Enable Cross-Origin Resource Sharing (CORS) for the server
+app.use(cors({
+    origin: true,
+    methods: [`GET`, `POST`, `DELETE`],
+    allowedHeaders: [`Content-Type`, `Authorization`]
+}));
+
+// Enable parsing of JSON
+app.use(express.json());
+
+// Middleware to pass control to the next handler function
+app.use((req, res, next) => {
+    next();
 });
 
-app.get('/example', (req, res) => {
-    res.send('Hello World');
-})
-
-app.listen(8080, () => {
-    console.log('Server Started on http://localhost:8080');
-    console.log('Press CTRL + C to stop server');
+// Start the server and log a message to the console
+app.listen(Port, () => {
+    console.log(`✅ Server is Running on ${Port} ✅`);
 });
-
-// You can now start your server by running: 'node server.js' in your terminal window.
-
-// initialize a node package.json file for your app
-// npm init
-
-// install express and save it to your package.json file
-// npm install express --save
